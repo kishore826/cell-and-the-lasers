7d9c9b86bf537eb0f5c42db5a4de5bba61478479
kishore826 <kishore826@gmail.com>
2026-08-26 20:41:07 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -24,6 +24,11 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         X = X + 70
         Cell.setPosition(X, 100)
         music.play(music.melodyPlayable(music.jumpUp), music.PlaybackMode.UntilDone)
+
+        control.simmessages.send(
+            "cell-lasers",
+            Buffer.fromUTF8("TEST_SCORE_123")
+        )
     }
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -336,10 +341,6 @@ let b = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Food)
 b.setPosition(150, 100)
-control.simmessages.send(
-    "cell-lasers",
-    Buffer.fromUTF8("TEST_FROM_MAKECODE")
-)
 game.onUpdateInterval(1000, function () {
     if (isAlive == true) {
         info.changeScoreBy(1)
