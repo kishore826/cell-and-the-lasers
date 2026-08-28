b4b2b5f889f788c0f0b087c754657279f6d8af23
kishore826 <kishore826@gmail.com>
2026-08-28 15:11:38 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,16 +2,6 @@ namespace SpriteKind {
     export const Ground = SpriteKind.create()
     export const P = SpriteKind.create()
 }
-function sendScoreToWebsite() {
-    const message = JSON.stringify({
-        score: info.score()
-    })
-
-    control.simmessages.send(
-        "cell-lasers",
-        Buffer.fromUTF8(message)
-    )
-}
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     if (X == 150 || X == 80) {
         X = X - 70
@@ -72,7 +62,6 @@ sprites.onOverlap(SpriteKind.P, SpriteKind.Player, function (sprite, otherSprite
         music.play(music.melodyPlayable(music.powerUp), music.PlaybackMode.UntilDone)
         game.splash("High-score = " + info.score())
     }
-    sendScoreToWebsite()
     game.gameOver(false)
 })
 let T = 0
@@ -347,7 +336,10 @@ let b = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Food)
 b.setPosition(150, 100)
-
+control.simmessages.send(
+    "cell-lasers",
+    Buffer.fromUTF8("TEST_FROM_MAKECODE")
+)
 game.onUpdateInterval(1000, function () {
     if (isAlive == true) {
         info.changeScoreBy(1)
@@ -359,7 +351,6 @@ forever(function () {
         isAlive = false
         game.splash("You have demon reflexes")
         game.splash("Made by MrLegend649", " Visit my channel on yt")
-        sendScoreToWebsite()
         game.gameOver(true)
     }
 })
