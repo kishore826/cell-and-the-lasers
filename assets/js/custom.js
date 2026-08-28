82a0f3703cd7e0ca5074a06d2406550cc39eabe6
kishore826 <kishore826@gmail.com>
2026-08-28 14:28:45 +0530

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,11 +1,19 @@
 /**
- * This will be loaded before starting the simulator.
- * If you wish to add custom javascript, 
- * ** make sure to add this line to pxt.json**
- * 
- *      "disableTargetTemplateFiles": true
- * 
- * otherwise MakeCode will override your changes.
- * 
- * To register a constrol simmessages, use addSimMessageHandler
+ * Cell & The Lasers - leaderboard bridge
  */
+
+addSimMessageHandler("cell-lasers", function (data) {
+    try {
+        console.log("MAKECODE MESSAGE RECEIVED:", data);
+
+        // Send the message from the GitHub Pages game
+        // to the outer game.html iframe.
+        window.parent.postMessage({
+            type: "cell-lasers-score",
+            score: data.score
+        }, "*");
+
+    } catch (error) {
+        console.error("CELL-LASERS MESSAGE ERROR:", error);
+    }
+});
